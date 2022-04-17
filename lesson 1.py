ab92d48cd944549d42c50459d57347083420d262
Liliya <lilaisavin@gmail.com>
2022-04-17 16:49:33 +0300

    Initial commit

---
+++ lesson 1.py
@@ -0,0 +1,58 @@
+# 1
+# name = 'Liliya'
+# surname = 'Isaenkova'
+# age = 35
+# print('Name:', name,'Surname:', surname,'Age:', age )
+# print(name, surname, age )
+# a = 5
+# b = 12
+# c = a+a
+# print( a + b - c )
+# 2
+# sec = int(input('введите время в секундах'))
+# print(sec // 3600)
+# print((sec % 3600) // 60)
+# print((sec // 3600)- 60)
+# hour = (sec // 3600)
+# min = ((sec % 3600) // 60)
+# sec2 = sec %  3600 % 60
+# print( hour,':', min, ':', sec2)
+# 3
+# number = int(input('введите число'))
+# number2 = int(number*11)
+# number3 = int(number*111)
+# print(number + number2 + number3)
+# 4
+# num = int(input('введите число'))
+# res = 0
+# while num > 0:
+#     lust_num = num % 10
+#     if res == 0:
+#         max_num = lust_num
+#     else:
+#         if max_num > lust_num:
+#             max_num = max_num
+#         else:
+#             max_num = lust_num
+#     num = num // 10
+#     res = res + 1
+# print('Максимальное число:', max_num)
+# 5
+# profit = int(input('выручка фирмы:'))
+# costs = int(input('расходы фирмы:'))
+# if profit > costs:
+#     print('Прибыль:', profit - costs)
+# else:
+#     costs > profit
+# print('Убытки:', costs- profit)
+# 6
+# emp = int(input('количество сотрудников:'))
+# profit = 900
+# revenue = 400
+# profitability = profit / revenue
+# print('рентабельность:', profitability)
+# emp1 = profit / emp
+# print('прибыль на сотрудника:', emp1)
+
+
+
